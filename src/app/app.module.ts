01cf1a85c73a53f3dfbb4468e84413d3881e4088
Keryanne <47926567+Keryanne@users.noreply.github.com>
2024-10-08 14:26:27 +0200

    Final Upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    RegistrationComponent,
-  ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        RegistrationComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        ReactiveFormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
